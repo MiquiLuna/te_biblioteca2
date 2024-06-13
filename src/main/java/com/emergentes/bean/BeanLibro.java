43b0ea39848a9790b200cf3c41bd603505e6f071
MiquiLuna <josuemiqui33@gmail.com>
2024-06-12 23:49:26 -0400

    version 1.1.1

---
+++ src/main/java/com/emergentes/bean/BeanLibro.java
@@ -0,0 +1,45 @@
+package com.emergentes.bean;
+
+import com.emergentes.entidades.Libro;
+import com.emergentes.jpa.LibroJpaController;
+import com.emergentes.jpa.exceptions.NonexistentEntityException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class BeanLibro {
+ private EntityManagerFactory emf;
+    private LibroJpaController jpaLibro;
+
+    public BeanLibro() {
+        emf = Persistence.createEntityManagerFactory("biblio20PU");
+        jpaLibro = new LibroJpaController(emf);
+    }
+    
+    public List<Libro> listarTodos(){
+        return jpaLibro.findLibroEntities();
+    }
+    public void insertar(Libro libro){
+        jpaLibro.create(libro);
+    }
+    public void editar(Libro libro){
+        try {
+            jpaLibro.edit(libro);
+        } catch (Exception ex) {
+            Logger.getLogger(BeanLibro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void eliminar(Integer id){
+        try {
+            jpaLibro.destroy(id);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(BeanLibro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public Libro buscar(Integer id){
+        return jpaLibro.findLibro(id);
+    }
+}   
+
---
+++ target/te_biblio2-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Biblioteca</h1>
+        Bienvenido a la biblioteca
+        <ul>
+            <li><a href="CategoriaServlet" >Categoria</a></li>
+            <li><a href="LibroServlet" >Libros</a></li>
+        </ul>
+    </body>
+</html>
